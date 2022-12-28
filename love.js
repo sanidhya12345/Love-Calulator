4a7d672053d616aa4e54bff80ed47d389d2cdd88
Sanidhya Varshney <60321589+sanidhya12345@users.noreply.github.com>
2022-12-28 17:12:30 +0530

    updated

--- love.js
+++ love.js
@@ -1,8 +1,20 @@
 var a=document.getElementById("boy")
 var b=document.getElementById("girl")
+var historyArray=[]
+function validate(){
+    if(a.value.length===0 || b.value.length===0){
+        return false;
+    }
+    return true;
+}
+
 function calculate(){
-    var n=Math.floor(Math.random()*100)+1;
-    alert(`Your calculate love percentage is ${n}%`)
-    a.value=""
-    b.value=""
+    if(validate()===true){
+        var n=Math.floor(Math.random()*100)+1;
+        alert(`Your calculate love percentage is ${n}%`)
+        historyArray.push(`boyname:- ${a.value} and girlname:- ${b.value} their love percentage is ${n}`);
+        console.log(historyArray)
+        a.value=""
+        b.value=""
+    }
 }
